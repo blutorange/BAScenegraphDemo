d16de2e95ae4aaec308c824ebc602eaf930183a2
Andre Wachsmuth <wachsmuth.andre@gmx.de>
2016-10-18 21:29:46 +0200

    removed function

--- ExerciseScenegraph/core/src/de/homelab/madgaksha/ba/mi15/cgca/scenegraph/ITemporalFunction.java
+++
@@ -1,5 +0,0 @@
-package de.homelab.madgaksha.ba.mi15.cgca.scenegraph;
-
-public interface ITemporalFunction {
-	public float apply(float x, float dxdt, float d2xdt2, float t);
-}
--- ExerciseScenegraph/core/src/de/homelab/madgaksha/ba/mi15/cgca/scenegraph/object/ChainController.java
+++ ExerciseScenegraph/core/src/de/homelab/madgaksha/ba/mi15/cgca/scenegraph/object/ChainController.java
@@ -2,8 +2,6 @@ package de.homelab.madgaksha.ba.mi15.cgca.scenegraph.object;
 
 import com.badlogic.gdx.math.MathUtils;
 
-import de.homelab.madgaksha.ba.mi15.cgca.scenegraph.ETemporalFunction;
-import de.homelab.madgaksha.ba.mi15.cgca.scenegraph.ITemporalFunction;
 import de.homelab.madgaksha.ba.mi15.cgca.scenegraph.game.Controller;
 import de.homelab.madgaksha.ba.mi15.cgca.scenegraph.graph.NodeAngle;
 
@@ -11,18 +9,16 @@ public class ChainController implements Controller {
 
 	private final float gravity;
 	private final float friction;
-	private final ITemporalFunction excitation;
 	private final Chain chain;
 
 	public ChainController(final Chain chain) {
-		this(chain, 200f, 0.5f, ETemporalFunction.IDENTITY);
+		this(chain, 200f, 0.5f);
 	}
 
-	public ChainController(final Chain chain, final float gravity, final float friction, final ITemporalFunction excitation) {
+	public ChainController(final Chain chain, final float gravity, final float friction) {
 		this.chain = chain;
 		this.gravity = gravity;
 		this.friction = friction;
-		this.excitation = excitation;
 	}
 
 	@Override
@@ -30,14 +26,15 @@ public class ChainController implements Controller {
 		simulate(time, deltaTime);
 	}
 
+	/**
+	 * @param time
+	 */
 	private void simulate(final float time, final float deltaTime) {
 		final float s = chain.nodes.size();
 		for (int i = 0; i < chain.nodes.size(); ++i) {
 			final NodeAngle node = chain.nodes.get(i);
 			node.angularAcceleration = -(s - i) * gravity * MathUtils.sinDeg(node.angle)
 					- friction * node.angularVelocity;
-			if (i == 0)
-				node.angularAcceleration += excitation.apply(node.angle, node.angularVelocity, node.angularAcceleration, time);
 			node.reset();
 			node.fromAcceleration(deltaTime);
 		}
